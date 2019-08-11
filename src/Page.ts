6f537caadb8d72ade0c93b7c26f852bdc1187d56
Richard Gieg <richgieg@users.noreply.github.com>
2019-08-10 19:09:13 -0700

    Add meta tags

--- src/Page.ts
+++ src/Page.ts
@@ -28,6 +28,8 @@ export abstract class Page {
         return `<!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${this.getFullTitle()}</title>
         <style>
             html {
