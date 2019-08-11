3c5eee53e7abf28ac4169b5001efe55ac34b613c
Richard Gieg <richgieg@users.noreply.github.com>
2019-08-10 23:21:02 -0700

    Update Index.ts

--- src/Root/Products/Index.ts
+++ src/Root/Products/Index.ts
@@ -1,11 +1,19 @@
 import { Page } from '../../Page';
+import { products } from './_Products';
 
 export class Index extends Page {
 
     public readonly title = '';
 
     protected getContent(): string {
-        return this.getFullTitle();
+        return `
+            <h1>${this.getFullTitle()}</h1>
+            <p>
+                ${this.link(products.pages.one, 'One')} |
+                ${this.link(products.pages.two, 'Two')} |
+                ${this.link(products.pages.three, 'Three')}
+            </p>
+        `;
     }
 
 }
